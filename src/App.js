fd522262ca76dcc501dfa753d3e7aa965f746d05
Argox2 <gaelrojotrujillo@gmail.com>
2022-05-06 07:48:22 -0500

    generate random password

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import Password from "./Password";
+import "./style.css";
+
+class App extends Component {
+    
+    render() {
+        return(
+            <div>
+                <p>PASSWORD GENERATOR TOOL</p>
+                <h1>Generate a secure password</h1>
+                <p>Use our online password generator to instantly create a secure, random password.</p>
+                <Password />
+                <h2>Customize your password</h2>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/Password.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+class Password extends Component {
+
+    generate_password = () => {
+        let password = ""
+        
+        for(let i = 0; i < 16; i++) {
+            let char = String.fromCharCode(Math.floor(Math.random() * (126 - 33 + 1) + 33));
+            password = password + char;
+        }
+
+        return password
+    }
+
+    render() {
+        return(
+            <div>
+                <p id="pass"> { this.generate_password() } </p>
+            </div>
+        )
+    }
+}
+
+export default Password;
